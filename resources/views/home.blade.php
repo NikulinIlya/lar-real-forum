71f7586edb1a1235fd37174533274d9575473e53
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-25 18:34:04 +0300

    feat(resources): make home vue component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Single Page Forum</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
         <v-app>
-
+            <app-home></app-home>
         </v-app>
     </div>
 
-    <script src="{{ assert('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
