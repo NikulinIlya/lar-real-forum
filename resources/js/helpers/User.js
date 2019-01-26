7fec4f86b7dee62a399bc15609da84dfc260365b
NikulinIlya <ilia.nikulin@gmail.com>
2019-01-26 16:20:49 +0300

    feat(resources): make token helper class

--- resources/js/helpers/User.js
+++ resources/js/helpers/User.js
@@ -1,7 +1,11 @@
+import Token from './Token'
+
 class User {
     login(data) {
         axios.post('/api/auth/login', data)
-            .then(res => console.log(res.data))
+            .then(res => {
+                Token.payload(res.data.access_token)
+                })
             .catch(error => console.log(error.response.data))
     }
 }
